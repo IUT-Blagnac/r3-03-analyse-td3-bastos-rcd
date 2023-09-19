d9c043ca1ae6abd2f65eb9d891aca2e11ef4e2a9
Bastien <record.bastien@gmail.com>
2023-09-19 16:43:06 +0200

    Test, code et javadoc

---
+++ main/src/main/java/main/App.java
@@ -0,0 +1,19 @@
+package main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+    }
+
+    /**
+     * Retourne une chaîne de caractère "Hello World!"
+     * 
+     * @return chaîne de caractère
+     */
+    public static String hello() {
+        return "Hello World!";
+    }
+}
---
+++ main/src/test/java/main/AppTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import main.App;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Test hello()
+     */
+    @Test
+    public void testHello() {
+        String test_result = "Hello World!";
+
+        assertEquals("ERREUR", test_result, App.hello());
+    }
+}
